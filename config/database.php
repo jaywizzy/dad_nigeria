534386447a2bd77de42ad394d9d7b9aad772c1b0
vanshergzie <jaywizzy34@gmail.com>
2018-03-27 08:27:00 +0100

    Add Heroku creds and update default route to hit the DB.

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -41,11 +48,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'dadnigeria'),
-            'username' => env('DB_USERNAME', 'vanshergzie'),
-            'password' => env('DB_PASSWORD', 'oluwashegun'),
+            // 'host' => env('DB_HOST', 'db4free.net'),            
+            // 'database' => env('DB_DATABASE', 'dadnigeria'),
+            // 'username' => env('DB_USERNAME', 'vanshergzie'),
+            // 'password' => env('DB_PASSWORD', 'oluwashegun'),
+            'host'      => $host,
+            'database'  => $database,
+            'username'  => $username,
+            'password'  => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
