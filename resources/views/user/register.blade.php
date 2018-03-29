edc62823b835cf790ebb9508247e77c0b439c2d9
vanshergzie <jaywizzy34@gmail.com>
2018-03-29 15:19:37 +0100

    changed DB

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -14,102 +14,66 @@
 
 <div class="container">
 	
-		<form action="{{route('store_user')}}" method="POST">
-			@if($errors)
-				<!-- <div class="alert alert-danger"> -->
-					<ul>
-						@foreach($errors->all() as $error)
-							<li>{{ $error }}</li>
-						@endforeach
-					</ul>
-				<!-- </div> -->
-			@endif
-				{{csrf_field()}}
+	@if($errors)
+		<!-- <div class="alert alert-danger"> -->
+			<ul>
+				@foreach($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+		<!-- </div> -->
+	@endif
+		{{csrf_field()}}
 
-				<!-- <label>USERNAME:</label><br>
-				<input type="text" name="username"><br><br> -->
-					
+	{!! Form::open(['url' => 'save', 'method' => 'post']) !!}
 
-				<div class="col-md-4 col-lg-4">
-					<div class="form-group">
-				
-					<label>FIRSTNAME:</label><br>
-					<input type="text" name="first_name" class="form-control">
-					</div>
-				</div>
-				<div class="col-md-4 col-lg-4">	
-					<div class="form-group">
-						<label>LASTNAME:</label><br>
-						<input type="text" name="last_name" class="form-control">
-					</div>
-				</div>
+		<div class="form-group">
+			{{ Form::label('first_name', 'First_name') }}
+			{{ Form::text('first_name', null, array('class' => 'form-control')) }}
+		</div>
 
-				<div class="col-md-4 col-lg-4">
-					<div class="form-group">
-					<label for="title">Title:<br>
-					<select name="title" class"form-control">
-						<option value="ms">Ms</option>
-						<option value="mrs">Mrs</option>
-						<option value="mr">Mr</option>
-					</select>
-					</div>
-					
-				</div>
+		<div class="form-group">
+			{{ Form::label('last_name', 'Last_name') }}
+			{{ Form::text('last_name', null, array('class' => 'form-control')) }}
+		</div>
 
-				<div class="col-md-4">
-					<div class="form-group">
-					<label>ORGANISATION:</label><br>
-					<input type="text" name="organisation" class"form-control"><br><br>
-					</div>
-					
-				</div>
-				
-				<div class="col-md-4">
-					<div class="form-group">
-				<label>DEPARTMENT:</label><br>
-				<input type="text" name="department" class"form-control"><br><br>
-					</div>
-				</div>
+		<div class="form-group">
+			{{ Form::label('title', 'Title') }}
+			{{ Form::text('title', null, array('class' => 'form-control')) }}
+		</div>
 
-				<div class="col-md-4">
-					<div class="form-group">
-				<label>POSITION:</label><br>
-				<input type="text" name="position"><br><br>
-					</div>
-				</div>
+		<div class="form-group">
+			{{ Form::label('organisation', 'Organisation') }}
+			{{ Form::text('organisation', null, array('class' => 'form-control')) }}
+		</div>
 
-				<div class="col-md-4">
-					<div class="form-group">
-				<label>POSTAL ADDRESS:</label><br>
-				<input type="text" name="postal_address"><br><br>
-					</div>
-				</div>
+		<div class="form-group">
+			{{ Form::label('department', 'Department') }}
+			{{ Form::text('department', null, array('class' => 'form-control')) }}
+		</div>
+		<div class="form-group">
+			{{ Form::label('position', 'Position') }}
+			{{ Form::text('position', null, array('class' => 'form-control')) }}
+		</div>
+		<div class="form-group">
+			{{ Form::label('postal_address', 'Postal_adress') }}
+			{{ Form::text('postal_address', null, array('class' => 'form-control')) }}
+		</div>
+		<div class="form-group">
+			{{ Form::label('phone', 'Phone') }}
+			{{ Form::text('phone', null, array('class' => 'form-control')) }}
+		</div>
+		<div class="form-group">
+			{{ Form::label('email', 'Email') }}
+			{{ Form::email('email', null, array('class' => 'form-control')) }}
+		</div>					
 
+		{!!Recaptcha::render()!!}<br><br>		
+			<!-- <div class="g-recaptcha" data-sitekey="6LeogU8UAAAAAMqL-CzfvGDWGEKn0wbXm6Gd6H9f"></div> -->
+	
 
-				<label>PHONE:</label><br>
-				<input type="number" name="phone"><br><br>
-
-				<label>EMAIL:</label><br>
-				<input type="email" name="email"><br><br>
-				
-				<label>COMMENT:</label><br>
-				<input type="comment" name="comment"><br><br>
-
-				<label>PASSWORD:</label><br>
-				<input type="password" name="password"><br><br>
-
-				<label>SECURITY IMAGE:</label>
-
-					{!!Recaptcha::render()!!}<br><br>		
-				<!-- <div class="g-recaptcha" data-sitekey="6LeogU8UAAAAAMqL-CzfvGDWGEKn0wbXm6Gd6H9f"></div> -->
-
-					
-
-				
-
-				<button type="submit" class="btn btn-primary" value="submit">submit</button>
-				<button type="reset" class="btn btn-success" >Cancel</button>
-			</form>
+			<button type="submit" class="btn btn-primary" value="submit">submit</button>
+			<button type="reset" class="btn btn-success" >Cancel</button>
 	</div>	
 </div>
 
