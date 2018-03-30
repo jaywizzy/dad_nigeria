a22537a00a93ef1e5416fff000e0523ef56413f7
vanshergzie <jaywizzy34@gmail.com>
2018-03-30 09:48:06 +0100

    using heroku PostgreSQL DB

--- config/database.php
+++ config/database.php
@@ -20,7 +20,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,16 +68,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            // 'host' => env('DB_HOST', '127.0.0.1'),
-            // 'port' => env('DB_PORT', '5432'),
-            // 'database' => env('DB_DATABASE', 'forge'),
-            // 'username' => env('DB_USERNAME', 'forge'),
-            // 'password' => env('DB_PASSWORD', ''),
-            'host'  =>  'ec2-107-21-126-193.compute-1.amazonaws.com',
-            'port'  =>  '5432',
-            'database'  =>  'dgekjfrum03p2',
-            'user'  =>  'hfiirjxuvbdbbw',
-            'password'  =>  '28ae9ad23c002bcd3cd6a0c9221ccb55cd135944fba681c9de9dcb8b479534fe',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            // 'host'  =>  'ec2-107-21-126-193.compute-1.amazonaws.com',
+            // 'port'  =>  '5432',
+            // 'database'  =>  'dgekjfrum03p2',
+            // 'user'  =>  'hfiirjxuvbdbbw',
+            // 'password'  =>  '28ae9ad23c002bcd3cd6a0c9221ccb55cd135944fba681c9de9dcb8b479534fe',
             
             'charset' => 'utf8',
             'prefix' => '',
