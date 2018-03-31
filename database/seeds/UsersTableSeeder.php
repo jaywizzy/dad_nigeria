aee8e13df8e6e4b833645a658c14a917381b4a4f
vanshergzie <jaywizzy34@gmail.com>
2018-03-31 16:50:58 +0100

    commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('users')->insert([
+            'first_name' => 'oluwasegun',
+            'last_name' => 'jay',
+            'email'    => 'jaywizzy34@gmail.com',
+            'organisation'    => 'Nhub',
+            'password' => Hash::make('awesome'),
+        ]);
+
+        //
+    }
+}
